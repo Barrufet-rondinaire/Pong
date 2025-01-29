eed9ec8eee44a53a3e5fc940f2179e2bf793d7ee
Xavier Sala <fsala2@cendrassos.net>
2025-01-29 11:02:00 +0100

    chore: posar namespace Pong

--- Pong/Pala.cs
+++ Pong/Pala.cs
@@ -5,19 +5,19 @@ namespace Pong;
 public class Pala
 {
     public Rectangle Posicio { get; private set; }
-    private int velocitat;
+    private readonly int _velocitat;
 
     public Pala(Rectangle posicioInicial, int velocitatInicial) 
     {
         Posicio = posicioInicial;
-        velocitat = velocitatInicial;
+        _velocitat = velocitatInicial;
     }
     public void Mou(Vector direccio, Rectangle midaPantalla)
     {
         if (direccio.X == 0 && direccio.Y == 0) return;
         
         var novaPosicio = Posicio;
-        novaPosicio.Offset(direccio * velocitat);
+        novaPosicio.Offset(direccio * _velocitat);
         if (midaPantalla.Contains(novaPosicio))
         {
             Posicio = novaPosicio;
